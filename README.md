9aef0e0c967b15f0aece4b1c259cde08ed23a41a
Richard <thespeedor@gmail.com>
2020-01-04 16:07:03 -0500

    added extra verify step

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # channelcrawlerWS
-webscraping channel crawler results
+
+# TODO 
+## use iframe to embed youtube video from dump.json 
+## add buttons to cycle through the videos
+## add remove and accept buttons
--- main.js
+++ main.js
@@ -1,93 +1,66 @@
 const {
     app,
-    BrowserWindow
+    BrowserWindow,
+    shell,
+    ipcMain
 } = require('electron');
 const fs = require('fs');
-const readline = require('readline');
-const {
-    google
-} = require('googleapis');
-const SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly'];
+const auth = require('./authorize');
+const TOKEN_PATH = 'token.json';
 let win;
-const tokenPath = 'token.json';
+let clientwin;
+let client;
+let data;
+let spreadsheetId;
 
-fs.readFile('credentials.json', (err, content) => {
-    if (err) return console.log('Error loading client secret file:', err);
-    // Authorize a client with credentials, then call the Google Sheets API.
-    authorize(JSON.parse(content), listMajors);
-});
+//open authorization url
+function openUrl() {
+    fs.readFile('credentials.json', (err, content) => {
+        if (err) return console.log('Error loading client secret file:', err);
+        // Authorize a client with credentials, then call the Google Sheets API.
+        client = auth.getClient(JSON.parse(content));
+        const url = auth.getAuthUrl(client);
+        shell.openExternal(url);
+    });
 
-function authorize(credentials, callback) {
-    const {
-        client_secret,
-        client_id,
-        redirect_uris
-    } = credentials.installed;
-    const oAuth2Client = new google.auth.OAuth2(
-        client_id, client_secret, redirect_uris[0]);
+}
 
-    // Check if we have previously stored a token.
-    fs.readFile(TOKEN_PATH, (err, token) => {
-        if (err) return getNewToken(oAuth2Client, callback);
-        oAuth2Client.setCredentials(JSON.parse(token));
-        callback(oAuth2Client);
+//load crawled data
+function loadData() {
+    fs.readFile('crawler/fetchcheckpoint/dump.json', (err, channels) => {
+        data = JSON.parse(channels);
+        console.log("Loaded data");
     });
 }
 
-function getNewToken(oAuth2Client, callback) {
-    const authUrl = oAuth2Client.generateAuthUrl({
-        access_type: 'offline',
-        scope: SCOPES,
-    });
-    console.log('Authorize this app by visiting this url:', authUrl);
-    const rl = readline.createInterface({
-        input: process.stdin,
-        output: process.stdout,
-    });
-    rl.question('Enter the code from that page here: ', (code) => {
-        rl.close();
-        oAuth2Client.getToken(code, (err, token) => {
-            if (err) return console.error('Error while trying to retrieve access token', err);
-            oAuth2Client.setCredentials(token);
-            // Store the token to disk for later program executions
-            fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
-                if (err) return console.error(err);
-                console.log('Token stored to', TOKEN_PATH);
-            });
-            callback(oAuth2Client);
-        });
-    });
-}
-
-/**
- * Prints the names and majors of students in a sample spreadsheet:
- * @see https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
- * @param {google.auth.OAuth2} auth The authenticated Google OAuth client.
- */
-function listMajors(auth) {
-    const sheets = google.sheets({
-        version: 'v4',
-        auth
-    });
-    sheets.spreadsheets.values.get({
-        spreadsheetId: '1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms',
-        range: 'Class Data!A2:E',
-    }, (err, res) => {
-        if (err) return console.log('The API returned an error: ' + err);
-        const rows = res.data.values;
-        if (rows.length) {
-            console.log('Name, Major:');
-            // Print columns A and E, which correspond to indices 0 and 4.
-            rows.map((row) => {
-                console.log(`${row[0]}, ${row[4]}`);
-            });
-        } else {
-            console.log('No data found.');
+//create the client window
+function createWindow() {
+    clientwin = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true
         }
     });
+    clientwin.loadFile('spreadsheet.html');
+    var code = `var ipcRenderer = require('electron').ipcRenderer;
+                var button = document.getElementById('spreadsheetsubmit');
+                button.addEventListener('click', () =>{
+                    ipcRenderer.send('sheetIdSubmit', document.getElementById('spreadsheetId').value);
+                });`;
+    clientwin.webContents.executeJavaScript(code);
+
+    clientwin.on('close', () => {
+        clientwin = null;
+    });
 }
 
-function createWindow() {
+//attach google sheets to the client page
+function attachSheets() {
+
+}
+//create the authorization window
+function createAuthWindow() {
     // Create the browser window.
     win = new BrowserWindow({
         width: 800,
@@ -95,25 +68,58 @@ function createWindow() {
         webPreferences: {
             nodeIntegration: true
         }
-    })
-
-    // and load the index.html of the app.
-    win.loadFile('index.html')
-
-    // Open the DevTools.
-    win.webContents.openDevTools()
-
+    });
+    win.loadFile('authenticate.html');
+    //open the auth url
+    openUrl();
+    var code = `
+                var ipcRenderer = require('electron').ipcRenderer;
+                var button = document.getElementById('submit');
+                button.addEventListener('click', () =>{
+                    var data = document.getElementById('authorizationField').value
+                    ipcRenderer.send('authSubmit', data);
+                });`;
+    //inject javascript into renderer
+    win.webContents.executeJavaScript(code);
     // Emitted when the window is closed.
     win.on('closed', () => {
         win = null
-    })
+    });
+
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+//authorize the user
+ipcMain.on('authSubmit', (event, args) => {
+    console.log(args);
+    auth.getToken(client, args);
+    createWindow();
+    win.close();
+});
 
+ipcMain.on('sheetIdSubmit', (event, args) => {
+    spreadsheetId = args;
+    clientwin.loadFile('index.html');
+    auth.getChannels(client, spreadsheetId);
+});
+//app.on('ready', createAuthWindow)
+app.on('ready', () => {
+    fs.readFile(TOKEN_PATH, (err, token) => {
+        //open auth window if client has not been verified
+        if (err) {
+            console.log("Token err");
+            createAuthWindow();
+        } else {
+            //read credentials file and authorize
+            fs.readFile('credentials.json', (err, content) => {
+                if (err) return console.log('Error loading client secret file:', err);
+                // Authorize a client with credentials, then call the Google Sheets API.
+                client = auth.getClient(JSON.parse(content));
+                client.setCredentials(JSON.parse(token));
+                createWindow();
+            });
+        }
+    });
+});
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') {
